886dbded9bd9d4989987de34b9750d0d2cbfab92
sanjayvs600 <127740824+sanjayvs600@users.noreply.github.com>
2025-02-05 11:38:59 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/ar
-    docker push ar8888/ar
+    docker login -u SANJAY V S -p sanjay@vs-600
+    docker tag test SANJAY V S/task2
+    docker push SANJAY V S/task2
     
